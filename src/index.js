698364b0b509e93baba0b0b864ac778e3b98c80f
Saida Zghal <zghal.saida@orange.fr>
2020-04-07 22:47:49 +0200

    mettre une majuscule à la première lettre de la todo

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ const addTodo = (text) => {
   text = text.trim();
   if (text) {
     todos.push({
-      text,
+      text: `${text[0].toUpperCase()}${text.slice(1)}`,
       done: false,
     });
     displayTodo();
